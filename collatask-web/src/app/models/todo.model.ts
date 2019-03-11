2b5427e8b58ef4d5adef2842ac53ca6f64aaa058
Nor Arriesgado <n.arriesgado@outlook.ph>
2019-03-11 19:17:40 +0800

    mock service

---
+++ collatask-web/src/app/models/todo.model.ts
@@ -0,0 +1,9 @@
+export class TodoModel {
+    todoId: string; // Guid
+    parentTodoId: string; // Guid
+    description: string;
+    addedBy: string; // email address
+    isCompleted: boolean;
+    dateAdded: Date;
+    sub: TodoModel[];
+}
