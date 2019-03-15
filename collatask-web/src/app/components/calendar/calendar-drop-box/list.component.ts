26420146ec2c5d52244887a1968182a4235583e6
Nor Arriesgado <n.arriesgado@outlook.ph>
2019-03-15 17:59:57 +0800

    ui revision

--- collatask-web/src/app/components/calendar/calendar-drop-box/list.component.ts
+++ collatask-web/src/app/components/calendar/calendar-drop-box/list.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { TodoService } from 'src/app/services/_index.service';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { TodoService, AuthService } from 'src/app/services/_index.service';
 import { TodoModel } from 'src/app/models/todo.model';
+import { DialogComponent, DialogData } from '../../dialog/dialog.component';
+import { EditTodoComponent } from '../../manage/_manage.component';
+import { MatDialog, MatSnackBar } from '@angular/material';
 
 @Component({
     selector: 'calendar-drop-box',
-    templateUrl: './list.component.html'
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.scss']
 })
 export class CalendarDropListComponent implements OnInit {
 
@@ -16,7 +21,11 @@ export class CalendarDropListComponent implements OnInit {
         this.lBoxDate = new Date(date);
     }
 
-    constructor(private todoService: TodoService) { }
+    constructor(private todoService: TodoService,
+        private authService: AuthService,
+        private snackbar: MatSnackBar,
+        private dialog: MatDialog,
+        private customDialog: DialogComponent) { }
 
     ngOnInit() {
         this.todoService.$_inProgress.subscribe(data => this.inProgress = data);
@@ -27,6 +36,48 @@ export class CalendarDropListComponent implements OnInit {
                     this.lTodosPerDay.push(data[i]);
             }
         });
+    }
+
+    drop(event: CdkDragDrop<string[]>) {
+        console.log(event);
+    }
+
+    // Possible duplicate code with home.component
+
+    btnInfo_Clicked(id: string) {
+        const _dialog = this.dialog.open(EditTodoComponent, { width: '450px', data: id, disableClose: true });
+        _dialog.afterOpened().subscribe(() => {
+            this.todoService.get(id);
+        });
+        _dialog.afterClosed().subscribe(() => { });
+    }
+
+    btnDelete_Clicked(id: string) {
+        this.customDialog.Prompt(new DialogData('Confirm', '', 'Are you sure you want to remove selected to-do?', null))
+            .afterClosed().subscribe(result => {
+                if (result) {
+                    this.todoService.remove(id, this.authService.getUserEmail()).then(result => {
+                        if (result !== undefined && result !== null) {
+                            if (!result.success)
+                                this.snackbar.open(result.message, null, { duration: 4000 });
+                        }
+                    });
+                }
+            });
+    }
+
+    btnComplete_Clicked(id: string) {
+        this.customDialog.Prompt(new DialogData('Confirm', '', 'Are you sure you want to complete selected to-do?', null))
+            .afterClosed().subscribe(result => {
+                if (result) {
+                    this.todoService.complete(id, this.authService.getUserEmail()).then(result => {
+                        if (result !== undefined && result !== null) {
+                            if (!result.success)
+                                this.snackbar.open(result.message, null, { duration: 4000 });
+                        }
+                    });
+                }
+            });
     }
 
 }
