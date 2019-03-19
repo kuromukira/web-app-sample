b797d0ddde0922fffdedcbf86fb56ca1c5874dd8
Nor Arriesgado <n.arriesgado@outlook.ph>
2019-03-19 14:04:45 +0800

    ui

--- collatask-react/src/components/login/sign-in/sign-in.component.js
+++ collatask-react/src/components/login/sign-in/sign-in.component.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Form, Button, ButtonGroup } from 'react-bootstrap';
+import { Form, Button, ButtonGroup, Row } from 'react-bootstrap';
 
 export default class SignInComponent extends React.Component {
     render() {
         return (
             <Form>
-                <Form.Group controlId="login.email">
+                <Form.Group as={Row} controlId="login.email">
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com"></Form.Control>
                 </Form.Group>
-                <Form.Group controlId="login.password">
+                <Form.Group as={Row} controlId="login.password">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Your password here"></Form.Control>
                 </Form.Group>
-                <ButtonGroup vertical className="d-flex">
+                <ButtonGroup as={Row} vertical className="d-flex">
                     <Button variant="outline-secondary" size="sm">SIGN-IN WITH GOOGLE</Button>
                     <Button variant="primary" size="sm" type="submit">SIGN-IN</Button>
                 </ButtonGroup>
--- collatask-react/src/components/login/sign-up/sign-up.component.js
+++ collatask-react/src/components/login/sign-up/sign-up.component.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Form, Button, ButtonGroup } from 'react-bootstrap';
+import { Form, Button, ButtonGroup, Row } from 'react-bootstrap';
 
 export default class SignUpComponent extends React.Component {
     render() {
         return (
             <Form>
-                <Form.Group controlId="register.email" required>
+                <Form.Group as={Row} controlId="register.email" required>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control type="email" placeholder="name@example.com"></Form.Control>
                 </Form.Group>
-                <Form.Group controlId="register.password" required>
+                <Form.Group as={Row} controlId="register.password" required>
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Must be 6 characters or more"></Form.Control>
                 </Form.Group>
-                <Form.Group controlId="register.confirmpw" required>
+                <Form.Group as={Row} controlId="register.confirmpw" required>
                     <Form.Label>Confirm Password</Form.Label>
                     <Form.Control type="password" placeholder="Confirm your password here"></Form.Control>
                 </Form.Group>
-                <ButtonGroup className="d-flex">
+                <ButtonGroup as={Row} className="d-flex">
                     <Button variant="primary" size="sm" type="submit">SIGN-UP</Button>
                 </ButtonGroup>
             </Form>
