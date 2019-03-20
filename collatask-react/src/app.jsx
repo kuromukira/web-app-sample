b87e4f5da5eed8fceea28813ea93f142ff1f6b48
Nor Arriesgado <n.arriesgado@outlook.ph>
2019-03-20 15:33:30 +0800

    session service

--- collatask-react/src/app.jsx
+++ collatask-react/src/app.jsx
@@ -9,37 +9,19 @@ import NavigationComponent from './components/navigation';
 import LandingPageComponent from './components/landing-page';
 import HomePageComponent from './components/home';
 import LoginPageComponent from './components/login';
-import { withFirebase } from './services/firebase';
+import { withAuthentication } from './services/session';
 
-class AppComponent extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { authUser: null }
-    }
-
-    componentDidMount() {
-        this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-            authUser ? this.setState({ authUser }) : this.setState({ authUser: null });
-        });
-    }
-
-    componentWillUnmount() {
-        this.listener();
-    }
-
-    render() {
-        return (
-            <Router>
-                <NavigationComponent authUser={this.state.authUser} />
-                <div className="main-body-padding">
-                    <Route exact path={routes.LANDING} component={LandingPageComponent} />
-                    <Route exact path={routes.HOME} component={HomePageComponent} />
-                    <Route exact path={routes.SIGN_IN} component={LoginPageComponent} />
-                </div>
-            </Router>
-        );
-    }
+function AppComponent() {
+    return (
+        <Router>
+            <NavigationComponent />
+            <div className="main-body-padding">
+                <Route exact path={routes.LANDING} component={LandingPageComponent} />
+                <Route exact path={routes.HOME} component={HomePageComponent} />
+                <Route exact path={routes.SIGN_IN} component={LoginPageComponent} />
+            </div>
+        </Router>
+    );
 }
 
-export default withFirebase(AppComponent);
+export default withAuthentication(AppComponent);
--- collatask-react/src/components/navigation/index.jsx
+++ collatask-react/src/components/navigation/index.jsx
@@ -3,19 +3,23 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import * as routes from '../../constants/routes';
 import SignOutComponent from '../login/sign-out';
+import { AuthUserContext } from '../../services/session';
 
-function NavigationComponent({ authUser }) {
+function NavigationComponent() {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand><Link className="navbar-brand" to={authUser ? routes.HOME : routes.LANDING}><strong>To-Do List</strong> App</Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    {authUser ? <Link className="nav-item nav-link" to={routes.HOME}>Home</Link> : <div></div>}
-                    {authUser ? <SignOutComponent></SignOutComponent> : <Link className="nav-item nav-link" to={routes.SIGN_IN}>Sign In</Link>}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <AuthUserContext.Consumer>{
+            authUser =>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand><Link className="navbar-brand" to={authUser ? routes.HOME : routes.LANDING}><strong>To-Do List</strong> App</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            {authUser ? <Link className="nav-item nav-link" to={routes.HOME}>Home</Link> : <div></div>}
+                            {authUser ? <SignOutComponent></SignOutComponent> : <Link className="nav-item nav-link" to={routes.SIGN_IN}>Sign In</Link>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+        }</AuthUserContext.Consumer>
     );
 }
 
