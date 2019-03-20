87d732ab45872e92404523af532776219c5d71fe
Nor Arriesgado <n.arriesgado@outlook.ph>
2019-03-20 14:39:15 +0800

    chang

--- collatask-react/src/components/login/sign-out/index.jsx
+++ collatask-react/src/components/login/sign-out/index.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import * as routes from '../../../constants/routes'
 import { withFirebase } from '../../../services/firebase';
 
-const SignOutComponent = ({ firebase }) => (
-    <Link className="nav-item nav-link" onClick={firebase.signOut} to={routes.SIGN_IN}>Sign Out</Link>
-)
+function SignOutComponent(firebase) {
+    return (
+        <Link className="nav-item nav-link" onClick={firebase.signOut} to={routes.SIGN_IN}>Sign Out</Link>
+    );
+}
 
 export default withFirebase(SignOutComponent);
--- collatask-react/src/components/navigation/index.jsx
+++ collatask-react/src/components/navigation/index.jsx
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 import * as routes from '../../constants/routes';
 import SignOutComponent from '../login/sign-out';
 
-export default class NavigationComponent extends React.Component {
-    render() {
-        return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand><Link className="navbar-brand" to={routes.LANDING}><strong>To-Do List</strong> App</Link></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Link className="nav-item nav-link" to={routes.HOME}>Home</Link>
-                        <Link className="nav-item nav-link" to={routes.SIGN_IN}>Sign In</Link>
-                        <SignOutComponent></SignOutComponent>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+function NavigationComponent() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand><Link className="navbar-brand" to={routes.LANDING}><strong>To-Do List</strong> App</Link></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link className="nav-item nav-link" to={routes.HOME}>Home</Link>
+                    <Link className="nav-item nav-link" to={routes.SIGN_IN}>Sign In</Link>
+                    <SignOutComponent></SignOutComponent>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
+
+export default NavigationComponent;
