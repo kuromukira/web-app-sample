24cb97beaa7ae555e5c02a74becb5be0f5e0b93e
Nor Arriesgado <n.arriesgado@outlook.ph>
2019-03-27 15:30:04 +0800

    edit todo

--- collatask-react/src/components/home/todo-list/modify/index.jsx
+++ collatask-react/src/components/home/todo-list/modify/index.jsx
@@ -1,25 +1,72 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import ManageDialogComponent from '../manage-dialog';
 import { withTodoService } from '../../../../services/todo';
+import { localStoreKeys } from '../../../../constants/config';
+
+const INITIAL_STATE = {
+    todoId: '',
+    description: '',
+    addedBy: '',
+    currentUser: localStorage.getItem(localStoreKeys.email),
+    todoDate: '',
+    isCompleted: false,
+    // not related to todos model
+    dialogShow: false,
+    error: null,
+    inProgress: false
+}
 
 class ModifyButtonComponent extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { dialogOpen: false, error: null, todo: null }
+        this.state = INITIAL_STATE;
     }
 
-    btnComplete_Clicked = () => this.setState({ dialogOpen: true });
+    onStateChange = (ev) => this.setState({ [ev.target.name]: ev.target.value, error: null, success: false, inProgress: false });
 
-    btnConfirm_Clicked = () => {
-        this.props.reloadTodos();
+    btnCloseDialog_Clicked = () => this.setState({ dialogShow: false, inProgress: false });
+
+    btnOpenDialog_Clicked = () => {
+        this.setState({ inProgress: true });
+        this.props.todoService.get(this.props.todoId)
+            .then((todo) => {
+                this.setState({
+                    dialogShow: true, error: null,
+                    todoId: todo.todoId,
+                    description: todo.description,
+                    addedBy: todo.addedBy,
+                    // todoDate: Date(todo.todoDate),
+                    isCompleted: todo.isCompleted,
+                });
+            })
+            .catch((error) => this.setState({ error }))
+            .finally(() => this.setState({ inProgress: false }))
+    }
+
+    btnSaveTodo_Clicked = (ev) => {
+        ev.preventDefault();
+        this.setState({ inProgress: true });
+        this.props.todoService.modify(this.state)
+            .then(() => {
+                this.props.reloadTodos();
+                this.setState(INITIAL_STATE)
+            })
+            .catch((error) => this.setState({ error }))
     }
 
     render() {
         return (
             <span>
-                <Button className="mx-auto" variant="outline-secondary" size="sm" onClick={this.btnComplete_Clicked}>Modify</Button>
-
+                <Button className="mx-auto" variant="outline-secondary" size="sm" onClick={this.btnOpenDialog_Clicked}>Modify</Button>
+                <ManageDialogComponent
+                    title="Edit To-Do"
+                    state={this.state}
+                    closeDialog={this.btnCloseDialog_Clicked}
+                    confirmDialog={this.btnSaveTodo_Clicked}
+                    fieldChange={this.onStateChange}>
+                </ManageDialogComponent>
             </span>
         );
     }
